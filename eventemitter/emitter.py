bd5b2669eefb99018e4a684aad3493362288eb7e
Zearin <Zearin@users.noreply.github.com>
2015-12-07 09:11:57 -0500

    Implement EventEmitter.remove_all_listeners()

--- eventemitter/emitter.py
+++ eventemitter/emitter.py
@@ -120,6 +120,20 @@ class EventEmitter:
 
         return False
 
+    def remove_all_listeners(self, event=None):
+        """Removes all listeners, or those of the specified *event*. 
+        
+        It's not a good idea to remove listeners that were added elsewhere in 
+        the code, especially when it's on an emitter that you didn't create 
+        (e.g. sockets or file streams).
+        """
+        if event is None:
+            self._listeners = collections.defaultdict(list)
+            self._once = collections.defaultdict(list)
+        else:
+            del self._listeners[event]
+            del self._once[event]
+
     @property
     def max_listeners(self):
         """Get the max number of listeners before warning."""
